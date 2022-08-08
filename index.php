d03ea3dc77e4531b407a408004961923569739ef
Aiosa <horakj7@gmail.com>
2022-08-08 10:12:01 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+define('FM_CONFIG', 'config.php');
+
+require './inc/files.php';
---
+++ js/viewerRun.js
@@ -0,0 +1,96 @@
+var user_settings = {
+    params: {
+        "debug": true
+    },
+    data: [],
+    background: [],
+    shaderSources: [],
+    visualizations: []
+};
+
+function setup(id) {
+
+}
+
+
+
+function moveTo(id) {
+    var elmnt = document.getElementById(id);
+    elmnt.scrollIntoView();
+}
+
+function go(newTab, title, image, ...dataArray) {
+    loadFormData(newTab, title, image, ...dataArray);
+    document.getElementById("redirect").submit();
+    resetFormData();
+}
+
+function goMask(newTab, title, image, maskJSONdata, ...dataArray) {
+    user_settings.plugins = {"gui_annotations": {}};
+    loadFormData(newTab, title, image, ...dataArray);
+    let form = document.getElementById("redirect");
+
+    var node = document.createElement("input");
+    node.setAttribute("type", "hidden");
+    node.setAttribute("name", `annotation-list`);
+    node.setAttribute("id", "annotation-list");
+
+    let annotData = {"version":"5.2.1","objects":[]};
+    maskJSONdata = maskData[maskJSONdata];
+    for (let annotation of maskJSONdata) {
+        annotData.objects.push({
+            "type":"rect","left":annotation.coord_x,"top":annotation.coord_y,"width":annotation.tile_w,"height":annotation.tile_w,
+            "presetID":1656486783423,"factoryId":"rect","sessionId":1656486783407,"layerId":1656486783423
+        });
+    }
+
+    node.setAttribute("value", JSON.stringify(annotData));
+    form.appendChild(node);node = document.createElement("input");
+    node.setAttribute("type", "hidden");
+    node.setAttribute("id", "annotation_presets");
+
+    node.setAttribute("name", `annotation_presets`);
+    node.setAttribute("value", `[{"color":"#ff0000","factoryID":"rect","presetID":1656486783423,"meta":{"category":{"name":"Category","value":""}}}]`);
+    form.appendChild(node);
+
+    form.submit();
+
+    document.getElementById('annotation-list').remove();
+    document.getElementById('annotation_presets').remove();
+    resetFormData();
+}
+
+function loadFormData(newTab, title, image, ...dataArray) {
+    let vis = {name: title, shaders: {}};
+    user_settings.data.push(image);
+    user_settings.background.push({
+        dataReference: 0,
+        lossless: false
+    });
+    let index = 0;
+    for (let item of dataArray) {
+        item.shader.dataReferences = [user_settings.data.length];
+        user_settings.data.push(item.data);
+        vis.shaders[index++] = item.shader;
+    }
+    user_settings.visualizations.push(vis);
+
+    document.getElementById("visualisation").value = JSON.stringify(user_settings);
+    if (newTab) {
+        document.getElementById("redirect").setAttribute("target", "_blank");
+    } else {
+        document.getElementById("redirect").removeAttribute("target");
+    }
+}
+
+function resetFormData() {
+    user_settings = {
+        params: {
+            "debug": true
+        },
+        data: [],
+        background: [],
+        shaderSources: [],
+        visualizations: []
+    };
+}
