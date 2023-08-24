c6f07c6ec6f0685be67afee21751564f65b11209
Aiosa <34658867+Aiosa@users.noreply.github.com>
2023-08-24 13:03:50 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,15 @@
 <?php
-//Most used configurations for overriding
 define('FM_DEBUG', false);
 define('FM_USE_AUTH', false);
 define('FM_ADVANCED_MODE', true);
-//define('FM_XO_DB_ROOT', "../xo_db/");
-define('FM_XO_DB_ROOT', null);
-//define('FM_WSI_ANALYSIS_PAGE', "/importer/server/analysis.php");
-define('FM_WSI_ANALYSIS_PAGE', null);
+define('FM_XO_DB_ROOT', null); //disabled
+define('FM_WSI_ANALYSIS_PAGE', null); //disabled
 define('FM_BROWSE_ROOT', '/var/www/data/');
 define('FM_XOPAT_URL', "/xopat/index.php");
 define('FM_XOPAT_SOURCES', '/xopat/user_setup.php');
 $image_preview_url_maker = function ($file) {
     return "/iipsrv/iipsrv.fcgi?Deepzoom={$file}_files/1/0_0.jpg";
 };
-
 define('FM_BROWSE_ROOT', '/');
 define('FM_IMAGE_SERVER_URL_PATH', FM_BROWSE_ROOT);
 define('FM_HTTP_PATH', FM_BROWSE_ROOT);
