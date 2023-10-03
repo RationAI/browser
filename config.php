f8363aa774468f3b7d80b4f7f0c1c9420eaae5a5
Aiosa <34658867+Aiosa@users.noreply.github.com>
2023-10-03 19:29:56 +0100

    PR discarded config.php differences

--- config.php
+++ config.php
@@ -1,9 +1,18 @@
 <?php
+function _read_env_browser($name, $default) {
+    $value = getenv($name);
+    if (empty($value) || $value == "") return $default;
+    return $value;
+}
+
+//Most used configurations for overriding
 define('FM_DEBUG', false);
-define('FM_USE_AUTH', false);
-define('FM_ADVANCED_MODE', true);
-define('FM_XO_DB_ROOT', null); //disabled
-define('FM_WSI_ANALYSIS_PAGE', null); //disabled
+define('FM_USE_AUTH', true);
+define('FM_ADVANCED_MODE', false);
+define('FM_XO_DB_ROOT', "../xo_db/");
+//define('FM_XO_DB_ROOT', null);
+define('FM_WSI_ANALYSIS_PAGE', _read_env_browser("XO_BROWSER_ANALYSIS", "/importer/server/analysis.php"));
+//define('FM_WSI_ANALYSIS_PAGE', null);
 define('FM_BROWSE_ROOT', '/var/www/data/');
 define('FM_XOPAT_URL', "/xopat/index.php");
 define('FM_XOPAT_SOURCES', '/xopat/user_setup.php');
@@ -11,5 +20,3 @@ define('FM_XOPAT_SOURCES', '/xopat/user_setup.php');
 $image_preview_url_maker = function ($file) {
     return "/iipsrv/iipsrv.fcgi?Deepzoom={$file}_files/1/0_0.jpg";
 };
-define('FM_IMAGE_SERVER_URL_PATH', FM_BROWSE_ROOT);
-define('FM_HTTP_PATH', FM_BROWSE_ROOT);
