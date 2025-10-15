9d8a35634af5c736495a8c6cf50c6624b782f110
Aiosa <34658867+Aiosa@users.noreply.github.com>
2025-10-15 11:28:53 +0200

    Update config.php
    Respect files root mount env variable

--- config.php
+++ config.php
@@ -1,12 +1,19 @@
 <?php
+
+function _read_env_browser($name, $default) {
+    $value = getenv($name);
+    if (empty($value) || $value == "") return $default;
+    return $value;
+}
+
 define('FM_DEBUG', false);
 define('FM_USE_AUTH', false);
 define('FM_ADVANCED_MODE', true);
 define('FM_XO_DB_ROOT', null); //disabled
 define('FM_WSI_ANALYSIS_PAGE', null); //disabled
-define('FM_BROWSE_ROOT', '/mnt/data/');
 define('FM_XOPAT_URL', "/xopat/index.php");
 define('FM_XOPAT_SOURCES', '/xopat/user_setup.php');
+define('FM_BROWSE_ROOT', _read_env_browser("XO_FILES_MOUNT", "/mnt/data/"));
 
 $image_preview_url_maker = function ($file) {
     return "/iipsrv/iipsrv.fcgi?Deepzoom={$file}_files/1/0_0.jpg";
