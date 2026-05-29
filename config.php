97cc8a71343f676f4059f0aee24c6cb5ce24d837
Aiosa <horakj7@gmail.com>
2026-05-29 12:12:13 +0200

    chore: configure viewer v3 url

--- config.php
+++ config.php
@@ -11,8 +11,7 @@ define('FM_USE_AUTH', false);
 define('FM_ADVANCED_MODE', true);
 define('FM_XO_DB_ROOT', null); //disabled
 define('FM_WSI_ANALYSIS_PAGE', null); //disabled
-define('FM_XOPAT_URL', "/xopat/index.php");
-define('FM_XOPAT_SOURCES', '/xopat/user_setup.php');
+define('FM_XOPAT_URL', "/v3/");
 define('FM_BROWSE_ROOT', _read_env_browser("XO_FILES_MOUNT", "/mnt/data/"));
 
 $image_preview_url_maker = function ($file) {
